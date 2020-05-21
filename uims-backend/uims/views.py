351f68b9f5c9c77173fe6191301bfb5022213ed0
PeriodicLaw <periodiclaw0120@gmail.com>
2020-05-21 16:50:44 +0800

    add deletion protect message

--- uims-backend/uims/views.py
+++ uims-backend/uims/views.py
@@ -1,44 +1,100 @@
 from django.shortcuts import render
+from django.db.models.deletion import ProtectedError
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.mixins import DestroyModelMixin
+from rest_framework.response import Response
+from rest_framework import status
 
 from . import models, serializers
 
 class CampusViewSet(ModelViewSet):
     queryset = models.Campus.objects.all()
     serializer_class = serializers.CampusSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(CampusViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class PersonViewSet(ModelViewSet):
     queryset = models.Person.objects.all()
     serializer_class = serializers.PersonSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(PersonViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class TeacherViewSet(ModelViewSet):
     queryset = models.Teacher.objects.all()
     serializer_class = serializers.TeacherSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(TeacherViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class MajorViewSet(ModelViewSet):
     queryset = models.Major.objects.all()
     serializer_class = serializers.MajorSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(MajorViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class ClassViewSet(ModelViewSet):
     queryset = models.Class.objects.all()
     serializer_class = serializers.ClassSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(ClassViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class StudentViewSet(ModelViewSet):
     queryset = models.Student.objects.all()
     serializer_class = serializers.StudentSerializer
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        print(instance)
+        try:
+            return super(StudentViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class CourseViewSet(ModelViewSet):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(CourseViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class CourseInfoViewSet(ModelViewSet):
     queryset = models.Course_Info.objects.all()
     serializer_class = serializers.CourseInfoSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(CourseInfoViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class CourseSelectionViewSet(ModelViewSet):
     queryset = models.Course_Selection.objects.all()
     serializer_class = serializers.CourseSelectionSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(CourseSelectionViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
 
 class StudentStatusChangeViewSet(ModelViewSet):
     queryset = models.Student_Status_Change.objects.all()
     serializer_class = serializers.StudentStatusChangeSerializer
+    def destroy(self, request, *args, **kwargs):
+        try:
+            return super(StudentStatusChangeViewSet, self).destroy(request, *args, **kwargs)
+        except ProtectedError as e:
+            return Response({"protected": [str(e)]}, status=status.HTTP_400_BAD_REQUEST)
